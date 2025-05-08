a146f3bc1f437ceab9b7f6e8ad1f51ffe1bf8576
LaneLR <laneleerichardson@gmail.com>
2025-05-08 12:16:24 -0500

    wrote Bag class and added tests for Bag class

---
+++ __tests__/classes/Bag.test.js
@@ -0,0 +1,36 @@
+// import Bag from "../../classes/Bag"
+const Bag = require('../../classes/Bag')
+// import Bag from "../../classes/Person"
+// import { describe, expect } from "@jest/globals"
+
+describe("Bag Class", () => {
+    // new Bag(weight, id, owner)
+    const bag1 = new Bag(5, 30, "John")
+    const bag2 = new Bag(2, 40)
+    const bag3 = new Bag(15, 20, "Ellie")
+
+
+    it("is an instance of Bag", () => {
+        expect(bag1).toBeInstanceOf(Bag)
+    })
+
+    it("has a weight and id" , () => {
+        expect(bag1.weight).toEqual(5)
+        expect(bag1.id).toEqual(30)
+    })
+
+    it("has an owner", () => {
+        expect(bag1.getOwner()).toEqual("John")
+        expect(bag2.getOwner()).toEqual(null)
+    })
+
+    it("getOwner() method gets owner data" , () => {
+        expect(bag1.getOwner()).toEqual("John")
+    })
+
+    it("assignOwner() method reassigns owner" , () => {
+        expect(bag1.assignOwner(bag3.getOwner())).toEqual("Ellie")
+    })
+
+
+})
